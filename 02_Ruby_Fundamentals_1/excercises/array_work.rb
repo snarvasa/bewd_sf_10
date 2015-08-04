963506f5ddc41faf9b4ec1a27e5baac41b3ebd26
Sean Narvasa <sean@snarvasa.com>
2015-08-03 22:18:34 -0700

    Array Work Excercises

---
+++ 02_Ruby_Fundamentals_1/excercises/array_work.rb
@@ -0,0 +1,14 @@
+rock_stars = ["Santana", "Santana", "Santana 1",  "Taylor Swift", "Katy Perry", "Michael Jackson", "Pharrell", "Megadeath"]
+
+#puts rock_stars
+#rock_stars << "Jerry Garcia"
+#rock_stars.push("Will Ferrell")
+#puts rock_stars.include?("Pharrell")
+#puts rock_stars.pop
+#puts rock_stars.size
+#puts rock_stars.sample
+#puts rock_stars.rotate
+puts rock_stars.uniq
+puts rock_stars.include?("Santana")
+
+puts "Is Santana included in this array? #{rock_stars.include?("Santana")}"
