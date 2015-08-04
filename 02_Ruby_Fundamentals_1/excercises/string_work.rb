2b19131de999100e174156d8df4dadc880f8f110
Sean Narvasa <sean@snarvasa.com>
2015-08-03 20:19:41 -0700

    completed string work

---
+++ 02_Ruby_Fundamentals_1/excercises/string_work.rb
@@ -0,0 +1,12 @@
+greeting = "Hey Girl, how YOU doin?"
+name = "Ryan Gosling"
+bewd_class = "Backend web development           "
+
+#puts greeting.length
+#puts greeting.prepend("You nice! ")
+#puts name << "That's dope!"
+#puts bewd_class.strip
+puts bewd_class.chars
+
+puts name + " says " + greeting
+puts "this is a class #{bewd_class.chars.class}"
